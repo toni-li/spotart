6f3d1bf96d027240980357f1a101cede56da4439
toni-li <toni.li@uwaterloo.ca>
2021-04-25 16:42:23 -0400

    no progress made lol

--- src/App.js
+++ src/App.js
@@ -7,36 +7,33 @@ import Cookies from 'js-cookie';
 import { SpotifyAuth, Scopes } from 'react-spotify-auth';
 import 'react-spotify-auth/dist/index.css';
 
-import { Track } from 'react-spotify-api'
-
 function App() {
   const token = Cookies.get('spotifyAuthToken')
   return (
     <div className="App">
-      <br></br>
-      <br></br>
-      <form>
-        <label for="url">Spotify URL:  </label>
-        <input type="text" id="url" name="url"></input>
-        <br></br>
-        <br></br>
-        <input type="submit" value="GO!"></input>
-      </form>
-
-      {token ? (
-        <SpotifyApiContext.Provider value={token}>
-          {/* Your Spotify Code here */}
-          <App />
-          {/* <p>You are authorized with token: {token}</p> */}
-        </SpotifyApiContext.Provider>
-      ) : (
+      <div id="login">
+        {token ? (
+          <SpotifyApiContext.Provider value={token}>
+            {/* Your Spotify Code here */}
+            <App />
+            {/* <p>You are authorized with token: {token}</p> */}
+          </SpotifyApiContext.Provider>
+        ) : (
           // Display the login page
           <SpotifyAuth
             redirectUri='http://localhost:3000/callback'
             clientID='f2a18728feb846ee96af7c0edee169b4'
             scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
           />
-      )}
+        )}
+      </div>
+
+      <form id="search">
+        <label for="url">Spotify URL:  </label>
+        <input type="text" id="url" name="url"></input> &nbsp;&nbsp;
+        <input type="submit" value="GO!"></input>
+      </form>
+
 
 
 
