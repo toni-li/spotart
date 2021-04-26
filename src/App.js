e5554c574ce0542138eb1a31673d33c3b260223f
toni-li <toni.li@uwaterloo.ca>
2021-04-25 20:25:15 -0400

    adding main page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { SpotifyApiContext } from 'react-spotify-api';
-import Cookies from 'js-cookie';
+// https://levelup.gitconnected.com/how-to-build-a-spotify-player-with-react-in-15-minutes-7e01991bc4b6
 
-import { SpotifyAuth, Scopes } from 'react-spotify-auth';
-import 'react-spotify-auth/dist/index.css';
+import React, { Component } from "react";
+import hash from "./hash";
+import "./App.css";
+import * as $ from "jquery";
 
-function App() {
-  const token = Cookies.get('spotifyAuthToken')
+import {
+  BrowserRouter as Router,
+  Route, Switch
+  } from "react-router-dom";
+
+import Main from "./components/Main"
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "f2a18728feb846ee96af7c0edee169b4";
+const redirectUri = "http://localhost:3000/callback";
+const scopes = [];
+
+class App extends Component {
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+  getTrackInfo(token, id) {
+    // Make a call using the token
+    $.ajax({
+      url: "	https://api.spotify.com/v1/tracks/" + id,
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        this.setState({
+          name: data.name,
+          artist: data.artists.name,
+        });
+      }
+    });
+  }
+render() {
   return (
-    <div className="App">
-      <div id="login">
-        {token ? (
-          <SpotifyApiContext.Provider value={token}>
-            {/* Your Spotify Code here */}
-            <App />
-            {/* <p>You are authorized with token: {token}</p> */}
-          </SpotifyApiContext.Provider>
-        ) : (
-          // Display the login page
-          <SpotifyAuth
-            redirectUri='http://localhost:3000/callback'
-            clientID='f2a18728feb846ee96af7c0edee169b4'
-            scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
-          />
-        )}
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+
+        </header>
+
+        <Switch>
+          <Route path="/callback" component={Main}/>
+          <Route path="/" component={App}/>
+        </Switch>
       </div>
-
-      <form id="search">
-        <label for="url">Spotify URL:  </label>
-        <input type="text" id="url" name="url"></input> &nbsp;&nbsp;
-        <input type="submit" value="GO!"></input>
-      </form>
-
-
-
-
-
-
-
-    </div>
+    </Router>
   );
+  }
 }
-
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    render() {
+        return (
+        <div id="main">
+            <p> stuff will go here </p>
+
+        </div>
+        )
+    }
+}
+
+export default Main;
